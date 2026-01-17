afc66b3555b55711647045eb0785d8daa49ac626
Alexandru Ţurcanu <pondorasti@gmail.com>
2026-01-17 14:25:26 -0800

    bump char to 1.0.9

--- Casks/char.rb
+++ Casks/char.rb
@@ -1,6 +1,6 @@
 cask "char" do
-  version "1.0.8"
-  sha256 "b95cb1804ce8ba7aaf3c024c1c6ae40185a1da8c9a89a85c3ed2125ebcf45699"
+  version "1.0.9"
+  sha256 "a4912416e7c1c9217ce92fc53da6077d8598103d6377ae2a0f3a516d965214ca"
 
   url "https://github.com/851-labs/char/releases/download/v#{version}/char.dmg"
   name "char"
