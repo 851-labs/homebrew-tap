2e957e0c372fc9adea0410378bb688291448c476
Alexandru Ţurcanu <pondorasti@gmail.com>
2026-01-18 19:17:51 -0800

    chore: bump macrack to 0.1.2

--- Formula/macrack.rb
+++ Formula/macrack.rb
@@ -1,8 +1,8 @@
 class Macrack < Formula
   desc "Keep your Mac server-ready"
   homepage "https://github.com/851-labs/macrack"
-  url "https://github.com/851-labs/macrack/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "fd6aa6ee48db9afb8698e0b6b2567d4edd9fda282f0f9215b45b848f201c5912"
+  url "https://github.com/851-labs/macrack/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "d0672038449ccc6f347e6bd47585b64fd6e35f81f7e7a586259560e74c7d9cdf"
   license "MIT"
 
   depends_on macos: :ventura
