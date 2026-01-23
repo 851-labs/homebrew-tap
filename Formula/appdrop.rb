f46729b72c800efd75eee1f6c87238ffbd518859
Alexandru Ţurcanu <pondorasti@gmail.com>
2026-01-22 17:27:21 -0800

    chore: bump appdrop to 0.1.26

--- Formula/appdrop.rb
+++ Formula/appdrop.rb
@@ -1,11 +1,11 @@
 class Appdrop < Formula
   desc "Zero-config macOS release CLI for building, signing, and notarizing apps"
   homepage "https://github.com/851-labs/appdrop"
-  version "0.1.25"
+  version "0.1.26"
 
   on_arm do
     url "https://github.com/851-labs/appdrop/releases/download/v#{version}/appdrop"
-    sha256 "0dcf7c6166424f54168a50f234b7f040c0c56499629479a186b734acc759baf5"
+    sha256 "48d2b46f09b963865d40abc487cc0f977f0c303180a5c581fb8ddff0cec1089c"
   end
 
   def install
