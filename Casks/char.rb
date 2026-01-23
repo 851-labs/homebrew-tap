3e72c5a1bd1ad627a93385c502d4e629e24fe68a
Alexandru Ţurcanu <pondorasti@gmail.com>
2026-01-22 17:02:51 -0800

    bump char to 1.0.46

--- Casks/char.rb
+++ Casks/char.rb
@@ -1,11 +1,11 @@
 cask "char" do
-  version "1.0.43"
-  sha256 "810b301e3e111f5a90301110806340688a69e86c58dffabc31fc28cb324c0afb"
+  version "1.0.46"
+  sha256 "d30bedd37d94dce9bd86c7d84aae9517a08951e9f0c7cd9e06f6f521a2dcedfa"
 
-  url "https://github.com/851-labs/char/releases/download/v#{version}/char.dmg"
-  name "char"
+  url "https://github.com/851-labs/Char/releases/download/v#{version}/char.dmg"
+  name "Char"
   desc "Word counter tool"
-  homepage "https://github.com/851-labs/char"
+  homepage "https://github.com/851-labs/Char"
   auto_updates true
 
   app "char.app"
