c87e9702707463dc39b1119a2520aac5924ff9a5
Alexandru Ţurcanu <pondorasti@gmail.com>
2026-01-23 17:13:12 -0800

    bump window-in-picture to 1.0.15

--- Casks/window-in-picture.rb
+++ Casks/window-in-picture.rb
@@ -1,12 +1,12 @@
 cask "window-in-picture" do
-  version "1.0.11"
-  sha256 "790ab0c57c4ab61bfe166f3831ef2dc0c2ea629a8236187774f309f633db8aa0"
+  version "1.0.15"
+  sha256 "4de2c1397dc4366282a8aed415e1e21ecbfc1f880aa0ec3b9cfd5db8f954abf9"
 
-  url "https://github.com/851-labs/Window-in-Picture/releases/download/v#{version}/WindowInPicture.dmg"
+  url "https://github.com/851-labs/Window-in-Picture/releases/download/v#{version}/Window-in-Picture.dmg"
   name "Window-in-Picture"
   desc "Menubar picture-in-picture window mirror"
   homepage "https://github.com/851-labs/Window-in-Picture"
   auto_updates true
 
-  app "WindowInPicture.app"
+  app "Window-in-Picture.app"
 end
