8bc6bd22dfb4ec36dbc76af602687136af4ea760
Alexandru Ţurcanu <pondorasti@gmail.com>
2026-01-27 15:18:09 -0800

    chore: bump window-in-picture to 1.0.27

--- Casks/window-in-picture.rb
+++ Casks/window-in-picture.rb
@@ -1,6 +1,6 @@
 cask "window-in-picture" do
-  version "1.0.26"
-  sha256 "c6bc25783bf3794fb328dfbd69341ef9ee770cc7c5ddb298e09afde735dac469"
+  version "1.0.27"
+  sha256 "3e5860aaec28ee19fe0548f553f32be11422f811de668e7df4c7b7f7e2cdd862"
 
   url "https://github.com/851-labs/Window-in-Picture/releases/download/v#{version}/Window-in-Picture.dmg"
   name "Window-in-Picture"
