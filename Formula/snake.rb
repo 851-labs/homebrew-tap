43fc078d4aa2c080c9b15cadc78c0b13a7172268
Alexandru Ţurcanu <pondorasti@gmail.com>
2026-02-03 13:38:55 -0800

    chore: update snake to 0.1.6

--- Formula/snake.rb
+++ Formula/snake.rb
@@ -1,30 +1,30 @@
 class Snake < Formula
   desc "Terminal Snake built with OpenTUI"
   homepage "https://github.com/851-labs/snake"
-  version "0.1.5"
+  version "0.1.6"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/851-labs/snake/releases/download/v#{version}/snake-darwin-x64.tar.gz"
-      sha256 "585f78328d1575714d4654939b430e8203db163c5947019499b08c8bc7bbcae8"
+      sha256 "5e93bac6788320d98429ef840700e1694ec1b09767d03cd6dde795324912e988"
     end
 
     on_arm do
       url "https://github.com/851-labs/snake/releases/download/v#{version}/snake-darwin-arm64.tar.gz"
-      sha256 "8b40bbca71546fe7e02dc0e8432e83f7a699bd964066da847dbd67a90e173081"
+      sha256 "1a1e84a7fa06aed97304a22ec89da30b1caeab5065e38d8eb2cb516a92668a89"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/851-labs/snake/releases/download/v#{version}/snake-linux-x64.tar.gz"
-      sha256 "f73f2cda882378cf0253c286f32f0cd9dcbb42c85ce2f2931e0cd919162510da"
+      sha256 "d81d3d557f60ba1ba2d7d0bbdca772a390a93dedabba88635f1f17483da848d7"
     end
 
     on_arm do
       url "https://github.com/851-labs/snake/releases/download/v#{version}/snake-linux-arm64.tar.gz"
-      sha256 "db9fca5dff375e58fe78092b6797a2be9c618534bbcc7b9f24236731779959e4"
+      sha256 "7bcbf28e3ea07dade0f2bd9e98f5a40b770036112871f607d81fc0c050fe8fb5"
     end
   end
 
